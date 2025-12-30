e59bc14164077b754cfbbb9044eaddef879ea2fc
kamol666 <ktoshpulatov17@gmail.com>
2025-12-30 15:26:27 +0500

    Click controller va service yangilandi

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const clickRoutes = require('./routes/click.routes');
 const clickController = require('./controllers/click.controller');
 const notFound = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
+const requestLogger = require('./middlewares/requestLogger');
 
 const app = express();
 
 // app.disable('x-powered-by'); // Commented out for now
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
+app.use(requestLogger);
 
 app.get('/healthz', (req, res) => res.json({ ok: true }));
 app.get('/pay', clickController.quickPay);
